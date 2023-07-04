c2a8228ea32c05c350fce9032347471852390afe
beckermr <becker.mr@gmail.com>
2023-07-04 06:54:23 -0500

    TST add tests for downloading

---
+++ tests/test_download.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+
+def test_download_help():
+    res = subprocess.run(
+        "des-archive-access-download --help",
+        shell=True,
+        check=True,
+        capture_output=True,
+    )
+    assert "usage: des-archive-access-download" in res.stdout.decode("utf-8")
+
+
+def test_download():
+    mloc = os.path.expanduser("~/.des_archive_access/metadata.db")
+    try:
+        res = subprocess.run(
+            "des-archive-access-download "
+            "--url "
+            "https://black.readthedocs.io/en/stable/"
+            "usage_and_configuration/the_basics.html",
+            shell=True,
+            check=True,
+            capture_output=True,
+        )
+        assert "downloading DB" in res.stderr.decode("utf-8")
+        assert os.path.exists(mloc)
+    finally:
+        subprocess.run(
+            "des-archive-access-download --remove",
+            shell=True,
+            check=True,
+            capture_output=True,
+        )
+
+        assert not os.path.exists(mloc)

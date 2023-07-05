06d5f42a7b18c1260fe8cc24c46be73044d0ea4f
beckermr <becker.mr@gmail.com>
2023-07-05 12:53:59 -0500

    ENH add most of the functions

--- des_archive_access/repl.py
+++ des_archive_access/repl.py
@@ -5,13 +5,31 @@ from click_repl import repl
 from prompt_toolkit.history import FileHistory
 
 from des_archive_access.dbfiles import get_des_archive_access_db_conn
+from des_archive_access.sql import parse_and_execute_query
 
 
 @click.group(invoke_without_command=True)
+@click.option("-l", "--loadsql", default=None, type=str)
+@click.option("-c", "--command", default=None, type=str)
 @click.pass_context
-def cli(ctx):
-    if ctx.invoked_subcommand is None:
-        ctx.invoke(sqlrepl)
+def cli(ctx, command, loadsql):
+    query = None
+
+    if command is not None:
+        query = command
+
+    if loadsql is not None:
+        with open(loadsql, "r") as fp:
+            query = fp.read()
+
+    if query is not None:
+        try:
+            parse_and_execute_query(query)
+        finally:
+            get_des_archive_access_db_conn().close()
+    else:
+        if ctx.invoked_subcommand is None:
+            ctx.invoke(sqlrepl)
 
 
 @cli.command()
@@ -27,14 +45,11 @@ def sqlrepl():
     try:
         repl(click.get_current_context(), prompt_kwargs=prompt_kwargs)
     finally:
-        conn = get_des_archive_access_db_conn()
-        conn.close()
+        get_des_archive_access_db_conn().close()
 
 
 @cli.command()
 @click.argument("query", nargs=-1, required=True)
 def sql(query):
-    conn = get_des_archive_access_db_conn()
     query = " ".join(query)
-    for row in conn.execute(query):
-        click.echo(row)
+    parse_and_execute_query(query)
